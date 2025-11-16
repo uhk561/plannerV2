cb9cbe7bc995612b7e1dcce82d911f844e6489da
DESKTOP-HIO8OC0\User <uhk561@naver.com>
2025-11-16 17:05:57 +0900

    깃 연동 테스트 및 리드미 파일 생성

---
+++ src/main/java/com/plannerv2/PlannerV2Application.java
@@ -0,0 +1,13 @@
+package com.plannerv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerV2Application.class, args);
+    }
+
+}
